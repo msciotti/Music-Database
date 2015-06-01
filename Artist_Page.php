e392fa1e69382a1f369feadcabcb8c5eedb983e6
Mason Sciotti <mason.sciotti@gmail.com>
2015-06-01 00:52:32 -0400

    Create Artist_Page.php

---
+++ Artist_Page.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Artist Page</title>
+</head>
+
+<style>
+img {
+	height: 300px;
+	width: 300px;
+	float: left;	
+}
+
+p {
+	color: white;
+	font-size: 35px;
+	font-family: Copperplate / Copperplate Gothic Light, sans-serif;
+	text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+	float: left;
+	
+}
+
+#other-albums {
+	margin-top: 150px;
+	height:150px;
+	width:150px;
+	margin-right: 10px;
+	display: block;
+	float: right;
+}
+
+#album-list {
+
+	color:white; 
+	font-size: 40px;
+	font-family: Copperplate / Copperplate Gothic Light, sans-serif;
+	text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+	margin-right: 50px;
+	float: right;
+	margin-bottom:-20%;
+	
+	
+}
+
+div.artist-albums {
+	float: right;
+	display: block;
+	position:relative;
+}
+</style>
+
+<body background="http://www.livemusictylertx.com/wp-content/uploads/2014/05/g_live_music.jpeg">
+
+
+<img src="http://i.forbesimg.com/media/lists/people/taylor-swift_416x416.jpg" />
+<p> Taylor Swift
+<br />
+Genre: Pop, Country </p>
+
+<p style="clear:both;"> Bio text goes here </p>
+
+<div class="artist-albums">
+<p id="album-list"> Albums by This Artist </p>
+
+<img id="other-albums"src="http://upload.wikimedia.org/wikipedia/en/8/8f/Taylor_Swift_-_Speak_Now_cover.png" />
+<img id="other-albums"src="http://upload.wikimedia.org/wikipedia/en/f/fa/Taylor_Swift.png" />
+<img id="other-albums"src="http://upload.wikimedia.org/wikipedia/en/8/86/Taylor_Swift_-_Fearless.png" />
+<img id="other-albums"src="http://upload.wikimedia.org/wikipedia/en/e/e8/Taylor_Swift_-_Red.png" />
+</div>
+
+</body>
+</html>
